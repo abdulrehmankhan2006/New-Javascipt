d071fc15a57610768f2816f93a9c175fd28fe474
Abdul Rehman Khan <145275109+abdulrehmankhan2006@users.noreply.github.com>
2024-02-07 14:36:46 +0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,129 @@
+
+//function sum(a,b) {
+ //   console.log(a+b);
+//}
+//function minus(a,b) {
+//    console.log(a-b);
+//}
+//function div(a,b) {
+//    console.log(a/b);
+//}
+//function mul(a,b) {
+//    console.log(a*b);
+//}
+
+//sum(10,80);
+//minus(10,8);
+//div(10,8);
+//mul(10,8);
+
+//let a = "90";
+//let b = 90;
+//console.log(a===90);
+
+//let a = "60";
+//let b = 50;
+//function sum(a,b) {
+//    if(a>b) {
+//        console.log(true);
+//
+//    } else{
+//        console.log(false);
+//    }
+//}
+
+
+
+//let age = 25
+//if (age>15) {
+//    console.log('Congrats');
+//} else if(age>15){
+//    console.log('Sorry');
+//} else{
+//    console.log('Invalid');
+//}
+
+//let driver_age = 16
+//if (driver_age>=18) {
+//    console.log('You Can Drive');
+//} else if(driver_age>15){
+//    console.log('You Cannot Drive');
+//} else{
+//    console.log('Invalid');
+//}
+
+//let cel = 32
+//let formula = cel*(9/6)+32
+//console.log(formula);
+ //let fer = 40
+// let formula = (fer-32)*(9/5)
+// console.log(formula);
+
+//GLOBAL VARIABLE
+
+// let formula;
+// function Degree(far) {
+//     let a;
+//     formula= (far-32)*(5/9);
+//     console.log(formula);
+// }
+// Degree(99.99)
+
+// let day = "Monday"
+// switch (day) {
+//     case "Monday":
+//         console.log("Raining");
+//         break;
+
+//     default:
+//         break;
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`3 X ${i} = ${3*i}`);
+    
+// }
+
+// let obj = {firstname:"Abdul Rehman" , age:"17"}
+// console.log(obj.firstname);
+// console.log(obj.age);
+
+// let arr = {"Abdul Rehman", 455,566,77}
+// console.log(arr);
+
+
+
+// if (obj.age == 22 && obj.firstname== "Abdul Rehman") {
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// let student_marks = [30,40,55,66]
+
+// for (let index = 0; index < student_marks.length; index++) {
+//     console.log(student_marks[index]);
+    
+// }
+
+
+// --------------TIME CONVERTER---------------
+
+// let sec = 20
+// let min = 40
+// function sum(mint) {
+//     return mint*60
+// }
+// console.log(sum(50));
+
+// function sum(hours) {
+//     return hours/(60*60)
+// }
+// console.log(sum(50));
+
+
+function sum(poly) {
+  let formula = 60*(3-2)*180
+  console.log(formula); 
+}
+sum()
---
+++ test.js
@@ -0,0 +1,140 @@
+// <-----QUESTION 01----->
+// let a = 60
+// let b = 40
+
+// if (a=b) {
+//     if (a=b) {
+//         console.log("True");
+//     }
+//     else{
+//         console.log("False");
+//     }
+// }
+
+
+// <-----QUESTION 02----->
+// let age = 17
+// if (age>=18) {
+//     console.log("Eligible");
+// } else if(age<=18) {
+//     console.log("Not Eligible                                                                                                                                                                                  ");
+// }
+
+// <---QUESTION 03--->
+// let day = 'Wednesday'
+// switch (day) {
+//     case "Wednesday":
+//         console.log("It's a sunny day");
+//         break;
+// case "Wednesday":
+//     console.log("It's not a sunny day");
+//     default:
+//         break;
+// }
+
+// // <---QUESTION 04--->
+// for (let i = 1; i < 10; i++) {
+//     console.log(i);
+    
+// }
+//<-----QUESTION 05----->
+// var fact = 9
+// var f=1
+// for (let i = 1; i <= fact; i++) {
+// f=f*i
+// console.log(f);    
+// }
+
+
+//<-----QUESTION 06----->
+
+
+
+
+//<-----QUESTION 07----->
+// let a = 20
+// let b = 30
+
+// function sum(a,b) {
+//    console.log(a+b);
+// }
+// function minus(a,b) {
+//    console.log(a-b);
+// }
+// function div(a,b) {
+//    console.log(a/b);
+// }
+// function mul(a,b) {
+//    console.log(a*b);
+// }
+
+// sum(10,90);
+// minus(10,6);
+// div(10,8);
+// mul(10,9);
+
+// <----QUESTION 08---->
+// let p = 100
+// if (p>=80) {
+//     console.log("A+");
+// }else if(p>=70){
+//    console.log("A");
+// }
+// else if(p>=60){
+//     console.log("B");
+// }
+// else if(p>=50){
+//     console.log("C");
+// }
+// else if(p>=40){
+//     console.log("D");
+// }
+// else if(p>=30){
+//     console.log("E");
+// }
+
+// else{
+//     console.log("F");
+// }
+
+
+// COMPULSORY QUESTION 1
+// <------AREA OF SQUARE ------>
+
+// let afs;
+// function sum(afs) {
+//     let area_of_square= afs*afs
+//     console.log(area_of_square);
+// }
+// sum(20)
+
+// <---AREA OF CIRCLE---
+
+
+// let formula;
+
+// function sum(afc) {
+//     let formula = afc*(3.14)*2*2
+//     console.log(formula);
+// }
+// sum(20)
+
+// <---AREA OF TRIANGLE--->
+
+// let formula;
+
+// function sum(aft) {
+//     let formula = aft*10/2
+//     console.log(formula);
+// }
+// sum(20)
+
+
+// COMPULSORY QUESTION 02
+
+
+
+// COMPULSORY QUESTION 03
+
+// <-----let aur var variable ki value reassigned hoti dusri values say aur const dusri values say reassigned nahi hoti---->
+
